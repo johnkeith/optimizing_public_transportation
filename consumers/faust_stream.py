60e8679773069a1f2475187fab16032119ef5bf5
John Keith <mrjohnkeith@gmail.com>
2020-08-07 14:19:17 -0400

    Faust working

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -3,9 +3,7 @@ import logging
 
 import faust
 
-
 logger = logging.getLogger(__name__)
-
 
 # Faust will ingest records from Kafka in this format
 class Station(faust.Record):
@@ -29,30 +27,45 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+topic = app.topic("postgres-connect-stations", value_type=Station)
+out_topic = app.topic("cta.stations", partitions=1)
+
+table = app.Table(
+   "cta.stations.table",
+   default=int,
+   partitions=5,
+   changelog_topic=out_topic
+)
+
+def convert_line(event: Station):
+    if event.blue:
+        return "blue"
+    elif event.red:
+        return "red"
+    elif event.green:
+        return "green"
+    else:
+        return None
 
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def process(stream):
 
+    async for event in stream:
+        line = convert_line(event)
+
+        if line is not None:
+            transformed_station = TransformedStation(
+                station_id=event.station_id,
+                station_name=event.station_name,
+                order=event.order,
+                line=line
+            )
+
+            table[event.station_id] = transformed_station
+
+            await out_topic.send(key=str(event.station_id), value=transformed_station)
 
 if __name__ == "__main__":
     app.main()
